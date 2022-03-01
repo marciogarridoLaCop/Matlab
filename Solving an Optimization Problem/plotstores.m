4a4ae3ff0e5f9b0a51e5fb2cbf2c2111d25b3074
Marcio <marciogarrido@id.uff.br>
2022-03-01 18:22:56 -0300

    PlotStore function draw store dynamic

--- Solving an Optimization Problem/plotstores.m
+++ Solving an Optimization Problem/plotstores.m
@@ -3,9 +3,9 @@ X = [];
 Y = [];
 X=x;
 Y=y;
-pgon1 = nsidedpoly(5,"Center",[X(1) Y(1)],"sidelength",3);
-pgon2 = nsidedpoly(5,"Center",[X(2) Y(2)],"sidelength",3);
-pgon3 = nsidedpoly(5,"Center",[X(3) Y(3)],"sidelength",3);
-plot([pgon1 pgon2 pgon3])
+    for i=1:length(x)
+    pgon(i) = nsidedpoly(5,"Center",[X(i) Y(i)],"sidelength",3);
+    end
+plot(pgon)
 axis equal
 end
